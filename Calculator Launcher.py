7662b8dd73fa5ba9a240b3e279849dee2717a089
Snake Amadeus <snake4y5h@gmail.com>
2022-01-09 02:46:06 +0800

    Update Calculator Launcher.py

--- Calculator Launcher.py
+++ Calculator Launcher.py
@@ -1,4 +1,4 @@
-import os
+import os, platform
 c = input("how many calculators would you like to open? :")
 for i in range(int(c) if c.isnumeric() else sum([ord(ch) for ch in c])):
-	os.system("calc")
+	os.system({"Windows" : "Calc", "Darwin" : "open -na Calculator &"}[platform.system()])

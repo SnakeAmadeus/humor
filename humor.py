e72639d71d9728f727fc641b916914bbd9983dfa
Snake Amadeus <snake4y5h@gmail.com>
2022-01-08 22:25:26 +0800

    handles user typing random craps

--- humor.py
+++ humor.py
@@ -1,3 +1,4 @@
 import os
-for i in range(int(input("how many calculators would you like to open? :"))):
+c = input("how many calculators would you like to open? :")
+for i in range(int(c) if c.isnumeric() else sum([ord(ch) for ch in c])):
 	os.system("calc")
